b2a1166d603795d8ee7403e1647cad039040e71d
Trịnh Văn Duy <49477836+Duytv081298@users.noreply.github.com>
2022-04-05 16:27:31 +0700

    púg

--- Assets/Scripts/Popup/CategorySelectedPopup.cs
+++ Assets/Scripts/Popup/CategorySelectedPopup.cs
@@ -6,54 +6,86 @@ using UnityEngine.UI;
 public class CategorySelectedPopup : MonoBehaviour
 {
 
-    [SerializeField] private Button btn_Close_X = null;
-    [SerializeField] private Button btn_Close = null;
-    [SerializeField] private Button btn_Casual_Play = null;
-    [SerializeField] private Button btn_Casual_Continue = null;
-    [SerializeField] private Button btn_Progress_Play_Next = null;
-    [SerializeField] private Button btn_Progress_Level = null;
+    // [SerializeField] private Button btn_Close_X = null;
+    // [SerializeField] private Button btn_Close = null;
+    // [SerializeField] private Button btn_Casual_Play = null;
+    // [SerializeField] private Button btn_Casual_Continue = null;
+    // [SerializeField] private Button btn_Progress_Play_Next = null;
+    // [SerializeField] private Button btn_Progress_Level = null;
 
-    // Start is called before the first frame update
+    [SerializeField] private CanvasGroup selectModeContainer = null;
+    [SerializeField] private CanvasGroup SelectDifficultyContainer = null;
     void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    { }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Debug.Log("click");
-            btn_Close_X.onClick.AddListener(ClosePopupCategorySelected);
-            btn_Close.onClick.AddListener(ClosePopupCategorySelected);
-            btn_Casual_Play.onClick.AddListener(PlayWithCategory);
-            btn_Casual_Continue.onClick.AddListener(ContinueWithCategory);
-            btn_Progress_Play_Next.onClick.AddListener(ShowProgressPlay);
-            btn_Progress_Level.onClick.AddListener(ShowProgressLevel);
-        }
-        
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     // Debug.Log("click");
+        //     // btn_Close_X.onClick.AddListener(ClosePopupCategorySelected);
+        //     // btn_Close.onClick.AddListener(ClosePopupCategorySelected);
+        //     // btn_Casual_Play.onClick.AddListener(PlayWithCategory);
+        //     // btn_Casual_Continue.onClick.AddListener(ContinueWithCategory);
+        //     // btn_Progress_Play_Next.onClick.AddListener(ShowProgressPlay);
+        //     // btn_Progress_Level.onClick.AddListener(ShowProgressLevel);
+        // }
+
     }
-    void ClosePopupCategorySelected()
+    public void ClosePopupCategorySelected()
     {
         PopupContainer.Instance.ClosePopup();
-        //Output this to console when Button1 or Button3 is clicked
         Debug.Log("You have clicked the button!");
     }
-    void PlayWithCategory()
+    void OpenDifficultyContainer()
+    {
+        selectModeContainer.interactable = false;
+        selectModeContainer.blocksRaycasts = false;
+        selectModeContainer.alpha = 0f;
+
+        SelectDifficultyContainer.interactable = true;
+        SelectDifficultyContainer.blocksRaycasts = true;
+        SelectDifficultyContainer.alpha = 1f;
+    }
+    void OpenModeContainer()
+    {
+        selectModeContainer.interactable = true;
+        selectModeContainer.blocksRaycasts = true;
+        selectModeContainer.alpha = 1f;
+
+        SelectDifficultyContainer.interactable = false;
+        SelectDifficultyContainer.blocksRaycasts = false;
+        SelectDifficultyContainer.alpha = 0f;
+    }
+    public void PlayWithCategory()
+    {
+        OpenDifficultyContainer();
+    }
+    public void ContinueWithCategory()
+    {
+        OpenDifficultyContainer();
+    }
+    public void ShowProgressPlay()
     {
 
     }
-    void ContinueWithCategory()
+    public void ShowProgressLevel()
     {
 
     }
-    void ShowProgressPlay()
-    {
 
-    }
-    void ShowProgressLevel()
+    public void OnDifficultySelected(int difficultyIndex)
     {
-
+        switch (difficultyIndex)
+        {
+            case 0:
+                Debug.Log("EasyButton");
+                break;
+            case 1:
+                Debug.Log("MediumButton");
+                break;
+            case 2:
+                Debug.Log("HardButton");
+                break;
+        }
     }
 }
